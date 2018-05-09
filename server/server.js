b28d50a0b8f884efcde24e242c8f2a292d1f86be
iseefirehy <zhanghongyu2011@gmail.com>
2018-05-08 23:12:33 -0700

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+
+const publicPath = path.join(__dirname,'../public');
+const port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+
+app.use(express.static(publicPath));
+
+app.listen(3000,()=>{
+   console.log(`Server is up on ${port}`);
+});
+
+
